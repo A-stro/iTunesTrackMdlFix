753729f1a100fb815127b814b46a1eea23a99dea
Aaron <consultant@Aarons-MacBook-Pro.local>
2019-08-14 20:40:29 -0400

    files update

---
+++ ItunesApp729/Model/Track.swift
@@ -0,0 +1,33 @@
+//
+//  Track.swift
+//  ItunesApp729
+//
+//  Created by mac on 8/14/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import Foundation
+
+struct TrackResponse: Decodable {
+    let results: [Track]
+}
+
+class Track: Decodable {
+    
+    let trackId: Int?
+    let artistName: String?
+    let trackViewUrl: String?
+    let trackTimeMillis: Int?
+    let price: Double?
+    
+    // Enum: Associated value or raw value
+    private enum CodingKeys: String, CodingKey {
+        case trackId
+        case artistName
+        case trackViewUrl
+        case trackTimeMillis
+        case price = "trackPrice"
+    }
+    
+    
+}
